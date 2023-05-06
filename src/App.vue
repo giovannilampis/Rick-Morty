57c12b5a03f4af2a69a5c21c726e8d5b2453efa9
giovannilampis <giolampis@hotmail.com>
2023-05-06 16:05:31 +0200

    add HeaderComponent to components folder

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script >
 
+import HeaderComponent from './components/HeaderComponent.vue'
+
+export default{
+  name: 'app',
+  components: {
+    HeaderComponent
+  }
+}
+
 </script>
 
 <template>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
