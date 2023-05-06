8e1bd6a941aee8be7f622c3436a332194f096bca
giovannilampis <giolampis@hotmail.com>
2023-05-06 18:20:02 +0200

    import SingleCharacter in CharactersComponent

--- src/components/CharactersComponent.vue
+++ src/components/CharactersComponent.vue
@@ -1,10 +1,13 @@
 <script>
 
 import { store } from '../store';
-
+import SingleCharacter from './SingleCharacter.vue'
 
 export default {
     name: 'CharactersComponent',
+    components: {
+        SingleCharacter
+    },
     data() {
         return {
             store
