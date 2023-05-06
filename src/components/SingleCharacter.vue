1962730448d6a9fc03121bf9c1daa97d002e21ee
giovannilampis <giolampis@hotmail.com>
2023-05-06 19:10:26 +0200

    add margin bottom to name & location name in cards

--- src/components/SingleCharacter.vue
+++ src/components/SingleCharacter.vue
@@ -8,8 +8,8 @@ export default {
 
 <template>
     <div class="text-center col-12 col-md-6 col-lg-4 mb-5">
-        <img class="rounded-circle" :src="detailsCharacter.image" :alt="detailsCharacter.name">
-        <h3>{{ detailsCharacter.name }}</h3>
+        <img class="rounded-circle mb-4" :src="detailsCharacter.image" :alt="detailsCharacter.name">
+        <h3 class="mb-4">{{ detailsCharacter.name }}</h3>
         <span>{{ detailsCharacter.location.name }}</span>
     </div>
 </template>
